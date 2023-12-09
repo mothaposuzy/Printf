cf5ddf85274210942889f3453e9aad59c2f4546d
mothaposuzy <mothaposuzy2@gmail.com>
2023-12-08 18:49:22 -0800

    0-task.c

---
+++ printf/0-task.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format.
+ * @format: is a character string.
+ * Returns: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	in char_to_print = 0;
+
+	va_list list;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(list, format);
+		while (*format)
+		{
+			if (*format != %)
+			{
+				write(1, format, 1);
+					char_to_print++;
+			}
+			else
+			{
+				format++;
+				if (format == '\0')
+					break;
+			}
+			if (*format == 'c')
+			{
+				char = va_arg(list, int)
+					write(1, &c. 1);
+					char_to_print++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(list, char*);
+				int strlen = 0;
+			}
+			while (str[str_len] != '\0')
+				strlen++;
+		}
+		write(1, str, str_len);
+		char_to_print += str_len;
+}
+else if (*format == '%')
+{
+	write(1, format, 1);
+	char_to_print++;
+}
+format++;
+}
+va_end(list);
+return (char_to_print);
+}
---
+++ printf/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+
+#endif
